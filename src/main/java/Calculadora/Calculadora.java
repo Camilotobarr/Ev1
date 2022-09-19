01a05d92d6580b692ad6ce4cfb818bd0b0305282
Camilo <Camilo@DESKTOP-K4MCJA2>
2022-09-19 02:12:34 -0300

    Corrección de int a float

---
+++ src/main/java/Calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Calculadora;
+
+/**
+ *
+ * @author Camilo
+ */
+public class Calculadora {
+
+  
+
+    public float resultado(float c, float i, float a){
+        
+       float resultado=((c*(i/100))*a);
+   return resultado; 
+   
+   
+    
+    }
+   
+      
+    
+}
---
+++ src/main/webapp/Inicial.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : Inicial
+    Created on : 18-09-2022, 23:59:31
+    Author     : Camilo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Calculadora</title>
+    </head>
+    <body>
+        <h1>Calculadora de Interes</h1>
+        
+    </body>
+</html>
